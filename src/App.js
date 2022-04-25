84e1f4abbf8af9c61617f70350179c7c9925528c
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-04-25 20:17:48 +0200

    Copy HTML or CSS code for APP React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+    <section className="conventer">
+        <h1>PRZELICZNIK WALUT</h1>
+        <p>Prosty przelicznik walut</p>
+        <form className="js-form">
+            <div className="currency">
+                <label for="iHave" className="currency__label">Mam</label>
+                <input id="ihave" type="number" min="1" max="10000000" step="0.01" className="currency__control js-valueCurrency" placeholder="Wpisz kwotę" autofocus/>
+                <select name="ihave" className="currency__control js-currencyIhave">
+                    <option value="pln" className="js-IhavePln" selected>PLN polski złoty</option>
+                    <option value="eur" className="js-IhaveEur">EUR euro</option>
+                    <option value="chf">CHF frank szwajcarski</option>
+                    <option value="usd">USD dolar amerykański</option>
+                </select>
+            </div>
+            <div className="currency">
+                <label for="iWillGet" className="currency__label">Otrzymam</label>
+                <input id="iWillGet" type="text" className="currency__control js-valueIwillGet" readonly/>
+                <select name="iWillGet" className="currency__control js-currencyIWillGet">
+                    <option value="pln" className="js-IWillGetPln">PLN polski złoty</option>
+                    <option value="eur" className="js-IWillGetEur" selected>EUR euro</option>
+                    <option value="chf">CHF frank szwajcarski</option>
+                    <option value="usd">USD dolar amerykański</option>
+                </select>
+            </div>
+        </form>
+    </section>
+    <footer className="footer">
+        <p>Wykonanie w ramach nauki programowania <a href="https://youcode.pl" className="footer__ink">YouCode</a></p>
+        <p>Photo by <a href="https://unsplash.com/@micheile?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Visual Stories || Micheile</a> on <a href="https://unsplash.com/s/photos/money?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </footer>
+    </>
   );
 }
 
