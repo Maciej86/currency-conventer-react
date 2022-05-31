688b68fe6b4f7764664515798c6432caadce084c
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-05-31 23:58:37 +0200

    Rename styled components

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,22 +1,22 @@
-import { StyledFooter, StyledFooterLink } from "./styled";
+import { StyledFooter, Link } from "./styled";
 
 const Footer = () => (
-	<StyledFooter>
-		<p>
-			Wykonanie w ramach nauki programowania{" "}
-			<StyledFooterLink href="https://youcode.pl">YouCode</StyledFooterLink>
-		</p>
-		<p>
-			Photo by{" "}
-			<a href="https://unsplash.com/@micheile?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-				Visual Stories || Micheile
-			</a>{" "}
-			on{" "}
-			<a href="https://unsplash.com/s/photos/money?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-				Unsplash
-			</a>
-		</p>
-	</StyledFooter>
+  <StyledFooter>
+    <p>
+      Wykonanie w ramach nauki programowania{" "}
+      <Link href="https://youcode.pl">YouCode</Link>
+    </p>
+    <p>
+      Photo by{" "}
+      <a href="https://unsplash.com/@micheile?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Visual Stories || Micheile
+      </a>{" "}
+      on{" "}
+      <a href="https://unsplash.com/s/photos/money?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Unsplash
+      </a>
+    </p>
+  </StyledFooter>
 );
 
 export default Footer;
--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-	margin: 40px 15px 0 15px;
-	padding: 15px 40px;
-	font-size: 14px;
-	background: rgb(255, 255, 255);
-	border-radius: 5px;
-	box-shadow: 0 0 15px rgb(69, 69, 69);
+  margin: 40px 15px 0 15px;
+  padding: 15px 40px;
+  font-size: 14px;
+  background: rgb(255, 255, 255);
+  border-radius: 5px;
+  box-shadow: 0 0 15px rgb(69, 69, 69);
 `;
 
-export const StyledFooterLink = styled.a`
-	color: rgb(100, 100, 100);
-	&:hover {
-		color: rgb(69, 69, 69);
-	}
+export const Link = styled.a`
+  color: rgb(100, 100, 100);
+  &:hover {
+    color: rgb(69, 69, 69);
+  }
 `;
