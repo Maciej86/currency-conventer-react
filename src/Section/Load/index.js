be5989a8846b38d757e26da9a1a7d52fab13dfe9
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-06-13 23:21:12 +0200

    Added alt for icon

--- src/Section/Load/index.js
+++ src/Section/Load/index.js
@@ -4,7 +4,7 @@ const Load = () => {
   return (
     <>
       <h3>Ładuję dane...</h3>
-      <img src={iconLoad} width="50" height="50" />
+      <img src={iconLoad} width="50" height="50" alt="ładowanie..." />
       <p>
         Kursy walut są pobierane z <b>Europejskiego Banku Centralnego</b>
       </p>
