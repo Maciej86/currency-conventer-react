e642385de4b419f6b89d8120c329565418a21f87
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-06-13 23:17:05 +0200

    Rename import icon

--- src/Section/Load/index.js
+++ src/Section/Load/index.js
@@ -1,10 +1,10 @@
-import loadSpiner from "./icon-load.webp";
+import iconLoad from "./icon-load.webp";
 
 const Load = () => {
   return (
     <>
       <h3>Ładuję dane...</h3>
-      <img src={loadSpiner} width="50" height="50" />
+      <img src={iconLoad} width="50" height="50" />
       <p>
         Kursy walut są pobierane z <b>Europejskiego Banku Centralnego</b>
       </p>
